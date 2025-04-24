290e875076b586f63f69a007f8bf387cebb81e9e
rudrali-kalyane <kalyanerudrali@gmail.com>
2025-04-24 21:13:03 +0530

    Initial commit of MangoPoint Django e-commerce project

---
+++ grocery_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Carousel)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(UserProfile)
+admin.site.register(Cart)
+admin.site.register(Booking)
+admin.site.register(Feedback)
