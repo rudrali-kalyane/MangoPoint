11d810d929ba06d9758af1536f1685539f6a64eb
rudrali-kalyane <kalyanerudrali@gmail.com>
2025-04-24 22:47:16 +0530

    Fix static files config for Render deployment

--- grocery_shop/settings.py
+++ grocery_shop/settings.py
@@ -116,11 +116,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'grocery_shop','static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'grocery_shop', 'static')]
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR, 'grocery_shop','static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
